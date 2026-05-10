a39371a2316794ae15856d0d7cbd516f59bbea2e
tushar-454 <imtushar454@gmail.com>
2026-05-10 20:01:20 +0600

    Setup ShadCN Component Library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-    return <div>Parfect Job</div>;
+    return (
+        <div className="flex min-h-svh items-center justify-center">
+            <Button>Parfect Match</Button>
+        </div>
+    );
 }
