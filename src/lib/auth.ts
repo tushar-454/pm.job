5c8f8f180a3446829b0309b933b9136e504fbecc
Tushar Imran <128408948+tushar-454@users.noreply.github.com>
2026-05-13 09:55:22 +0600

    feat(auth): refactor database initialization for betterAuth; streamline schema integration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,17 +1,18 @@
 import { getDB } from "@/db";
+import * as authSchema from "@/db/auth-schema";
 import * as schema from "@/db/schema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 
-const getAuthDB = async () => await getDB();
+const authDb = await getDB();
 
 export const auth = betterAuth({
-    database: drizzleAdapter(getAuthDB, {
+    database: drizzleAdapter(authDb, {
         provider: "pg",
         schema: {
+            ...authSchema,
             ...schema,
-            user: schema.users,
         },
         usePlural: true,
     }),
